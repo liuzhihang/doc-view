0b85d52a4e71870a9284c14c806031e084e02ae4
liuzhihang <liuzhihangs@qq.com>
2021-10-25 21:06:15 +0800

    调整代码

--- src/main/java/com/liuzhihang/doc/view/DocViewBundle.java
+++ src/main/java/com/liuzhihang/doc/view/DocViewBundle.java
@@ -2,12 +2,9 @@
 package com.liuzhihang.doc.view;
 
 import com.intellij.DynamicBundle;
-import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.PropertyKey;
-
-import java.util.function.Supplier;
 
 public final class DocViewBundle extends DynamicBundle {
 
@@ -20,13 +17,7 @@ public final class DocViewBundle extends DynamicBundle {
     }
 
     @NotNull
-    public static @Nls
-    String message(@NotNull @PropertyKey(resourceBundle = BUNDLE) String key, Object... params) {
+    public static String message(@NotNull @PropertyKey(resourceBundle = BUNDLE) String key, Object... params) {
         return INSTANCE.getMessage(key, params);
-    }
-
-    @NotNull
-    public static Supplier<String> messagePointer(@NotNull @PropertyKey(resourceBundle = BUNDLE) String key, Object... params) {
-        return INSTANCE.getLazyMessage(key, params);
     }
 }
